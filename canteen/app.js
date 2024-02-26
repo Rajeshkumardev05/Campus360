a36f89ee256a0453690ca56615b7e570ad60ca4c
Rajesh kumar <139973789+Rajeshkumardev05@users.noreply.github.com>
2024-02-26 19:51:54 +0530

    1st commit

---
+++ canteen/app.js
@@ -0,0 +1,127 @@
+let openShopping = document.querySelector('.shopping');
+let closeShopping = document.querySelector('.closeShopping');
+let list = document.querySelector('.list');
+let listCard = document.querySelector('.listCard');
+let body = document.querySelector('body');
+let total = document.querySelector('.total');
+let quantity = document.querySelector('.quantity');
+
+openShopping.addEventListener('click', ()=>{
+    body.classList.add('active');
+})
+closeShopping.addEventListener('click', ()=>{
+    body.classList.remove('active');
+})
+
+let products = [
+    {
+        id: 1,
+        name: 'Chole Bhature',
+        image: 'chole_bhature.PNG',
+        price:  50,
+        width:'200px'
+    },
+    {
+        id: 2,
+        name: 'Pav Bhaji',
+        image: 'pav_bhaji.JPEG',
+        price: 50
+    },
+    {
+        id: 3,
+        name: 'Noodles',
+        image: 'noodles.PNG',
+        price: 30
+    },
+    {
+        id: 4,
+        name: 'Veg Burger',
+        image: 'burger.PNG',
+        price: 40
+    },
+    {
+        id: 5,
+        name: 'Dosa Sambhar',
+        image: 'dosa.PNG',
+        price: 50
+    },
+    {
+        id: 6,
+        name: 'Aaloo Parantha',
+        image: 'aaloo_parantha.PNG',
+        price: 30
+    },
+    {
+        id: 7,
+        name: 'Momos',
+        image: 'momos.PNG',
+        price: 30
+    },
+    {
+        id: 8,
+        name: 'Paties',
+        image: 'paties.PNG',
+        price: 30
+    },
+    {
+        id: 9,
+        name: 'Chana Samosa',
+        image: 'chana_samosa.PNG',
+        price: 30
+    }
+];
+let listCards  = [];
+function initApp(){
+    products.forEach((value, key) =>{
+        let newDiv = document.createElement('div');
+        newDiv.classList.add('item');
+        newDiv.innerHTML = `
+            <img src="image/${value.image}">
+            <div class="title">${value.name}</div>
+            <div class="price">${value.price.toLocaleString()}</div>
+            <button onclick="addToCard(${key})">Add To Card</button>`;
+        list.appendChild(newDiv);
+    })
+}
+initApp();
+function addToCard(key){
+    if(listCards[key] == null){
+        // copy product form list to list card
+        listCards[key] = JSON.parse(JSON.stringify(products[key]));
+        listCards[key].quantity = 1;
+    }
+    reloadCard();
+}
+function reloadCard(){
+    listCard.innerHTML = '';
+    let count = 0;
+    let totalPrice = 0;
+    listCards.forEach((value, key)=>{
+        totalPrice += value.price;
+        count += value.quantity;
+        if(value != null){
+            let newDiv = document.createElement('li');
+            newDiv.innerHTML = `
+                <div><img src="image/${value.image}"/></div>
+                <div>${value.name}</div>
+                <div>${value.price.toLocaleString()}</div>
+                <div>
+                    <button onclick="changeQuantity(${key}, ${value.quantity - 1})">-</button>
+                    <div class="count">${value.quantity}</div>
+                    <button onclick="changeQuantity(${key}, ${value.quantity + 1})">+</button>
+                </div>`;
+                listCard.appendChild(newDiv);
+        }
+    })
+    total.innerText = totalPrice.toLocaleString();
+    quantity.innerText = count;
+}
+function changeQuantity(key, quantity){
+    if(quantity == 0){
+        delete listCards[key];
+    }else{
+        listCards[key].quantity = quantity;
+        listCards[key].price = quantity * products[key].price;
+    }
+    reloadCard();
+}
